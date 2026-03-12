3d46bdf3d7a0c8e63cd8f799fddd522f5f9d2205
Adrian Radores <aradores@repay.ph>
2026-03-13 05:03:59 +0800

    add new

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.g.have_nerd_font = false
+vim.o.laststatus = 3
+vim.o.fillchars = "eob: "
 
 do
   local deprecate = vim.deprecate
--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,12 +1,13 @@
 return {
   'tpope/vim-fugitive',
+  lazy = false,
   -- Optional: add keymaps
   keys = {
     { '<leader>gs', '<cmd>Git<cr>', desc = 'Git status' },
     { '<leader>gc', '<cmd>Git commit<cr>', desc = 'Git commit' },
     { '<leader>gp', '<cmd>Git push<cr>', desc = 'Git push' },
     { '<leader>gl', '<cmd>Git pull<cr>', desc = 'Git pull' },
-    { '<leader>gd', '<cmd>Gdiffsplit<cr>', desc = 'Git diff split' } ,
+    { '<leader>gd', '<cmd>Gdiffsplit<cr>', desc = 'Git diff split' },
     { '<leader>gb', '<cmd>Git blame<cr>', desc = 'Git blame' },
   },
 }
